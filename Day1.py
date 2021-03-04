162b6bada76da8e4b60029a727cde7f3d8f53efb
thesakshipandey <54810583+thesakshipandey@users.noreply.github.com>
2021-03-04 10:38:23 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,4 @@
+print("WELCOME TO BAND NAME GENERATOR")
+city = input("Enter the name of your city\n")
+pet = input("Enter the name of your Pet\n")
+print("YOUR BAND NAME COULD BE " + city + " " + pet)
